427140a39f64157eea98e096c90907da0b407731
Uluka Prince <74175430+princeuluka@users.noreply.github.com>
2022-09-14 07:10:52 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - function to print
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+char _putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	unsigned int c;
 
-	for (c = 0; c < sizeof(holberton); c++)
+	for (c = 0; c < sizeof(_putchar); c++)
 	{
-		_putchar(holberton[c]);
+		_putchar(_putchar[c]);
 	}
 _putchar('\n');
 return (0);
