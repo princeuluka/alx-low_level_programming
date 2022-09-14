1c41cd70d938e0fb14eb86c8352f99300d2b4044
Uluka Prince <74175430+princeuluka@users.noreply.github.com>
2022-09-14 07:13:47 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-char _putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+char word[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	unsigned int c;
 
-	for (c = 0; c < sizeof(_putchar); c++)
+	for (c = 0; c < sizeof(word); c++)
 	{
-		_putchar(_putchar[c]);
+		_putchar(word[c]);
 	}
 _putchar('\n');
 return (0);
