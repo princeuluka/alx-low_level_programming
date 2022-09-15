31527eb67922a2b2af4b77c60e99238ca472e4c2
Uluka Prince <74175430+princeuluka@users.noreply.github.com>
2022-09-15 12:21:14 +0100

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul - function
+* @a: first operand
+* @b: second operand
+*
+* Decription: Multiply two integers.
+* Return: Always 0(success)
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
