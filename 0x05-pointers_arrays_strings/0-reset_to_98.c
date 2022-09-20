91543763b3f054a26dbaf7024e221070e846507a
Uluka Prince <74175430+princeuluka@users.noreply.github.com>
2022-09-20 07:03:29 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
 #include "main.h"
 /**
-* reset_to_98 - function
-* @n: pointer
-*
-* Description: function takes pointer to an int param and updates value
-* Return: Always 0
-*/
+ * reset_to_98 - check the code for Alx School students.
+ * @n: declaration of n and paramters for the function reset_to_98
+ * Return: Always 0.
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
