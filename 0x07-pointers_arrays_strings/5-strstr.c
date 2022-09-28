cf00c7407d0849f70231ad54f9ecd9fec334ae7f
Uluka Prince <74175430+princeuluka@users.noreply.github.com>
2022-09-28 11:34:25 +0100

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#define NULL 0
+/**
+* _strstr - function
+* @haystack: string to search in
+* @needle: substring to find in string
+*
+* Description: function that locates a substring
+* Return: pointer to begin at substring or null
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int c = 0;
+	int c2 = 0;
+
+	if (needle[0] == '\0')
+	{
+		return (&haystack[0]);
+	}
+
+	while (haystack[c])
+	{
+		if (haystack[c] == needle[c2])
+		{
+			c++;
+			c2++;
+
+			if (needle[c2] == '\0')
+				return (&haystack[c - c2]);
+		}
+		else
+		{
+			c++;
+			c = (c - c2);
+			c2 = 0;
+		}
+	}
+	return ('\0');
+}
