a3b04146f79030474c5b0321d2f588ea45820892
Uluka Prince <74175430+princeuluka@users.noreply.github.com>
2022-09-28 11:33:51 +0100

    Create 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#define NULL 0
+/**
+* _strpbrk - function
+* @s: string to search in first  occur
+* @accept: string parameter set
+*
+* Description: function that searches a string for any of a set of bytes.
+* Return: pointer to type s or null
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (NULL);
+
+	return (0);
+}
