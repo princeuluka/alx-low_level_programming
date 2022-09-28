1790d4cc6380da06618add6dce54e0bfb454f32c
Uluka Prince <74175430+princeuluka@users.noreply.github.com>
2022-09-28 12:09:20 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,36 @@
 #include "main.h"
+
 /**
-* _sqrt_recursion - function
-* @n: integer to get square root of
-*
-* Description: function to find sqroot of given integer
-* Return: 0, return -1 if no square root.
-*/
+ *_sqrt_recursion -  func that returns the natural square root of a number.
+ * @n: for number input
+ * Return: 0
+ */
+
 int _sqrt_recursion(int n)
 {
-	int c = 0;
-
-	if (c == n)
-	{
+	if (n == 0)
 		return (0);
-	}
-	return (0);
+	else if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (helper(n, 1));
+}
+
+/**
+ * helper - blank
+ * @n: number
+ * @i: incrementor
+ * Return: -1;
+ */
+
+int helper(int n, int i)
+{
+	if (n == (i * i))
+		return (i);
+	else if (n > (i * i))
+		return (helper(n, i + 1));
+	else
+		return (-1);
 }
