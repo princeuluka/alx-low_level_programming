5f0783609e5aa9ad04a6143bd3b2a1a995d5ea05
Uluka Prince <74175430+princeuluka@users.noreply.github.com>
2022-09-28 11:30:53 +0100

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#define NULL 0
+/**
+* _strchr - function
+* @s: string to search in
+* @c: char to return
+*
+* Description: function to locate a character in a string
+* Return: pointer with first c or NULL if none
+*/
+char *_strchr(char *s, char c)
+{
+	for (;; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	if (*s == c && c == NULL)
+		return (s);
+	if (*s == NULL)
+		break;
+	}
+	return (NULL);
+}
