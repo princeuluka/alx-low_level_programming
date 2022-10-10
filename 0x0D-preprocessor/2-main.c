177b91bf974eb105d62c0165b4cd52800a3b36c4
Uluka Prince <74175430+princeuluka@users.noreply.github.com>
2022-10-11 00:33:27 +0100

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - program
+* Description: print name if file it was compiled from.
+* Return: Always 0
+*/
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+
+	return (0);
+}
