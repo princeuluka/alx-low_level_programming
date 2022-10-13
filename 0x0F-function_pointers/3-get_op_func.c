e4eb07a12cb42a46e48fd009314716d88eee5e97
Uluka Prince <74175430+princeuluka@users.noreply.github.com>
2022-10-13 06:48:40 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,48 +1,34 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
-<<<<<<< HEAD
- * get_op_func - ...
- * @s: ...
- *
- * Return: ...
- */
-=======
-  * get_op_func - ...
-  * @s: ...
-  *
-  * Return: ...
-  */
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
+ * get_op_func - selects the correct function
+ * to perform the operation asked by the user
+ * @s: pointer to a char that operator
+ * passed as argument to the program the name
+ * of the operation
+ * Return: a pointer to the function that corresponds
+ * to the operator given as a parameter
+*/
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-<<<<<<< HEAD
-	{ "/", op_div }
-=======
-	{ "/", op_div },
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
-	{ "%", op_mod },
-	{ NULL, NULL }
-	};
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+
 	int i = 0;
 
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
-<<<<<<< HEAD
-		
-=======
-
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
+		if (ops[i].operator[0] == s[0])
+			return ((*ops[i].proto));
 		i++;
 	}
 
-	return (0);
+	return (NULL);
 }
