a7b09827d9ae59a5125515bab0b0b1af39faf141
Uluka Prince <74175430+princeuluka@users.noreply.github.com>
2022-10-13 06:48:13 +0100

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,22 +1,22 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef CALC_H
+#define CALC_H
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct op -  Is the structure of operations
+ * @operator: operator
+ * @proto: operation prototype
  */
 typedef struct op
 {
-	char *op;
-	int (*f)(int a, int b);
+	char *operator;
+	int (*proto)(int, int);
 } op_t;
 
+int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+
 #endif
