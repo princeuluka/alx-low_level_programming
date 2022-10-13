4955af379fa05aba60da40f2eea8a83aae67e813
Uluka Prince <74175430+princeuluka@users.noreply.github.com>
2022-10-13 06:49:23 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,73 +3,71 @@
 #include <stdio.h>
 
 /**
- * op_add - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
- */
+ * op_add - adds 2 numbers
+ * @a: int number
+ * @b: int number
+ * Return: a + b
+*/
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
- */
+ * op_sub - substracts 2 numbers
+ * @a: int number
+ * @b: int number
+ * Return: a - b
+*/
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - ...
- * @a: ...
- * @b: ...
- *
- *  Return: ...
- */
+ * op_mul - Multiplies 2 numbers
+ * @a: int number
+ * @b: int number
+ * Return: a * b
+*/
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - ...
- * @a: ...
- * @b: ...
- *
- *  Return: ...
- */
+ * op_div - Divides 2 numbers
+ * @a: int number
+ * @b: int number
+ * Return: a / b
+*/
+
 int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error\n");
+		printf("%s\n", "Error");
 		exit(100);
 	}
-
 	return (a / b);
 }
 
 /**
- * op_mod - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
- */
+ * op_mod - Finds de rest of a division
+ * @a: int number
+ * @b: int number
+ * Return: a % b
+*/
+
 int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error\n");
+		printf("%s\n", "Error");
 		exit(100);
 	}
-
 	return (a % b);
 }
